2e4d51513919707bb9df5915bb1086b277676c59
Jesse Nelson <spheromak@gmail.com>
2013-03-02 21:33:40 -0800

    update with bag name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 
 default[:dns][:forwarders] = [ ]
 default[:dns][:zones] = [ ]
+default[:dns][:bag_name] = "dns_zones"
 
 # default self to master (to be overriden in env or role)
 default[:dns][:master] = ipaddress
